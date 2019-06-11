700d5389f167d0b36fd2e5943fff59acd950870f
chris <frantzyy@yahoo.com>
2019-06-11 09:54:49 -0600

    brought in tests and create initial InMemoryTimeEntryREspository

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,26 @@
+package io.pivotal.pal.tracker;
+
+public class InMemoryTimeEntryRepository {
+
+
+    public TimeEntry create(TimeEntry timeEntry) {
+
+
+    }
+
+    public TimeEntry update(int id, TimeEntry timeEntry){
+
+    }
+
+    public TimeEntry find(int id) {
+
+
+    }
+
+    public static void delete(int id){
+
+    }
+
+    public boolean list() {
+    }
+}
---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -0,0 +1,9 @@
+package io.pivotal.pal.tracker;
+
+import java.time.LocalDate;
+
+public class TimeEntry {
+
+    public TimeEntry(long projectId, long userId, LocalDate parse, int id) {
+    }
+}
