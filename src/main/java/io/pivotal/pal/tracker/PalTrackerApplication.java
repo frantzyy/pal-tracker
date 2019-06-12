98e6ba0552e29ee453feb7e2035f0456279a924e
chris <frantzyy@yahoo.com>
2019-06-12 13:14:17 -0600

    fixed datasource connection

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -13,9 +15,11 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public JdbcTimeEntryRepository jdbcTimeEntryRepository(){
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    public JdbcTimeEntryRepository jdbcTimeEntryRepository(DataSource dataSource){
+//        MysqlDataSource dataSource = new MysqlDataSource();
+//        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+
+
 
         return new JdbcTimeEntryRepository(dataSource);
     }
